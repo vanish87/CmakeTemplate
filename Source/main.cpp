58347e6e291a498c2260f2111fd2aa86eec30eec
Li Yuan <liyuan@team-lab.com>
2019-04-24 20:05:29 +0900

    linux compile passed

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,7 +4,7 @@
 #include "lua.h"
 #include "ft2build.h"
 #include FT_FREETYPE_H
-#include "FX11/d3dx11effect.h"
+//#include "FX11/d3dx11effect.h"
 
 
 using namespace std;
@@ -12,8 +12,8 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     cout<<"Hello World"<<endl;
-    aiVector2D vectortest;
-    D3DX11_EFFECT_DESC desc;
+  //  aiVector2D vectortest;
+    //D3DX11_EFFECT_DESC desc;
     
 	FT_Library  library;
 	FT_Face     face;
