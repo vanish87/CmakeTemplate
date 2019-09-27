64ecca6747ace4a4fe2db3638db7eb5d8f06ebe3
Li Yuan <liyuan@team-lab.com>
2019-09-27 11:58:02 +0900

    add comment

--- BuildExternalLibs.py
+++ BuildExternalLibs.py
@@ -3,8 +3,11 @@ import os
 buildPath="Build/External"
 if not os.path.exists(buildPath):
 	os.makedirs(buildPath, exist_ok=True)
+
+#build debug
 subprocess.run(["cmake", "../../Source/ExternalLibs", "-DCMAKE_BUILD_TYPE=Debug", "-DCMAKE_GENERATOR_PLATFORM=x64"], cwd=buildPath)
 subprocess.run(["cmake", "--build", ".", "--target", "install", "--config", "Debug"], cwd=buildPath)
+#build release
 subprocess.run(["cmake", "../../Source/ExternalLibs", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_GENERATOR_PLATFORM=x64"], cwd=buildPath)
 subprocess.run(["cmake", "--build", ".", "--target", "install", "--config", "Release"], cwd=buildPath)
 os.system("pause")
